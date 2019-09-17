476d2e35ee3e8f04b9666491cec48e8a22389290
Ethan Dye <42674340+deddye@users.noreply.github.com>
2019-09-16 23:04:03 -0500

    Add files via upload
    Late night updates to the UI, if anyone wants to help make the buttons better feel free.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-let x = 10;
+function changeTab(tabName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(tabName).style.display = "inline";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();

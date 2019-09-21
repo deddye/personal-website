a3587a97b13a626ecd04ad4037d6e89d95ac7fa5
Ethan Dye <42674340+deddye@users.noreply.github.com>
2019-09-21 18:26:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function changeTab(tabName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
+
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
 
@@ -7,18 +7,14 @@ function changeTab(tabName, elmnt, color) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
 
-  // Show the specific tab content
   document.getElementById(tabName).style.display = "block";
 
-  // Add the specific color to the button used to open the tab content
   elmnt.style.backgroundColor = color;
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
